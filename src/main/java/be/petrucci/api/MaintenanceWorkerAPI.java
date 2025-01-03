566b779f1a1e63e0d9e4c529a500e17790ad2fe0
Zeetwy <cornil.antoine.coran@gmail.com>
2025-01-03 14:23:34 +0100

    Implemented see work in progress for `MaintenanceWorker`.

--- src/main/java/be/petrucci/api/MaintenanceWorkerAPI.java
+++ src/main/java/be/petrucci/api/MaintenanceWorkerAPI.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -21,6 +22,18 @@ public class MaintenanceWorkerAPI {
 		return Response
 				.status(Status.OK)
 				.entity(workers)
+				.build();
+	}
+
+	@GET
+	@Path("{id}/work")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getWorkerWorkInProgress(@PathParam("id") int worker_id) {
+		MaintenanceWorker worker = new MaintenanceWorker();
+		worker.setId(worker_id);
+		return Response
+				.status(Status.OK)
+				.entity(worker.seeWorkInProgress())
 				.build();
 	}
 }
